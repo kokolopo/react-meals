af3aba56c58a5799867dfee0315fffa02f9ab648
kokolopo <kokolopo321@gmail.com>
2023-02-04 14:00:49 +0700

    setUp

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        {/* <img src="#" alt="gambar" /> */}
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
